d38d62e9582cf5a5ae0918a799f6f647caa47914
shunhamm <shun.022885@gmail.com>
2022-03-08 02:15:04 +0900

    first commit

---
+++ api/routers/task.py
@@ -0,0 +1,25 @@
+from typing import List
+from urllib import response
+from fastapi import APIRouter
+import api.schemas.task as task_schema
+
+
+router = APIRouter()
+
+
+@router.get("/tasks", response_model=List[task_schema.Task])
+async def list_tasks():
+    return [task_schema.Task(id=1, title="First Todo task")]
+
+
+@router.post("/tasks", response_model=task_schema.TaskCreateResponse)
+async def create_task(task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
+
+@router.put("/tasks/{task_id}")
+async def update_task():
+    pass
+
+@router.delete("/tasks/{task_id}")
+async def delete_tasks():
+    pass
---
+++ api/schemas/task.py
@@ -0,0 +1,20 @@
+from doctest import Example
+from typing import Optional
+from pydantic import BaseModel, Field
+
+
+class TaskBase(BaseModel):
+    title = Optional[str] = Field(None, example="Pick up clothese")
+
+class TaskCreate(TaskBase):
+    pass
+
+class TaskCreateResponse(TaskCreate):
+    id: int
+
+class Task(TaskBase):
+    id: int
+    done: bool = Field(False, description="完了フラグ")
+
+class Config:
+    orm_mode = True
