2b4e1a4028a8accb5321cf21aa386793e96e825e
shunhamm <shun.022885@gmail.com>
2022-03-18 12:14:50 +0900

    add delete function

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,6 +1,7 @@
+from http.client import HTTPException
 from typing import List
 from urllib import response
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.cruds.task as task_crud
@@ -22,9 +23,19 @@ async def create_task(
     return await task_crud.create_task(db, task_body)
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
-async def update_task(task_id: int, task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
+async def update_task(
+    task_id: int, task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
+):
+    task = await task_crud.get_task(db, task_id=task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return await task_crud.update_task(db, task_body, original=task)
 
 @router.delete("/tasks/{task_id}", response_model=None)
-async def delete_task(task_id: int):
-    return 
+async def delete_task(task_id: int, db: AsyncSession = Depends(get_db)):
+    task = await task_crud.get_task(db, task_id=task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    return await task_crud.delete_task(db, original=task)
